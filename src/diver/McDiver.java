2abd027b67abf32ff533860cfa0a4afda755e301
Batchpingu <lamle96@gmail.com>
2023-07-22 18:10:11 -0400

    Broken aPath that calculate path base on manhattan distance

--- src/diver/McDiver.java
+++ src/diver/McDiver.java
@@ -57,10 +57,10 @@ public class McDiver implements SewerDiver {
         // Create list of pointers for neighbors
         List<NodeStatus> sortedNode = new ArrayList<>(s.neighbors());
         Collections.sort(sortedNode);
-        System.out.println("Current: " + current);
-        System.out.println("Size: " + sortedNode.size());
+        //System.out.println("Current: " + current);
+        //System.out.println("Size: " + sortedNode.size());
         for(NodeStatus n: sortedNode){
-            System.out.println(n.getId() + " " + n.getDistanceToRing());
+            //System.out.println(n.getId() + " " + n.getDistanceToRing());
         }
         for (NodeStatus neighbor : sortedNode) {
             if (!visited.contains(neighbor.getId())) {
@@ -82,7 +82,8 @@ public class McDiver implements SewerDiver {
         // DO NOT WRITE ALL THE CODE HERE. Instead, write your method elsewhere,
         // with a good specification, and call it from this one.
         //basicScram(state);
-        maxScramGreedy(state);
+        //maxScramGreedy(state);
+        aPath(state);
     }
     /**
      * Helper method used by scram() that uses dijkstra's to walk McDiver to exit along the
@@ -220,6 +221,51 @@ public class McDiver implements SewerDiver {
                 s.moveTo(e.destination());
             }
         }
+    }
+    public static void aPath(ScramState s){
+        // Initialize a map for 0(1) lookup of cost of each node
+        Map<Node, Integer> aMap = new HashMap<>();
+
+        // Initialize each node as key and its cost as the value
+        for(Node node: s.allNodes()){
+            aMap.put(node, nodeCost(s.currentNode(),s.exit(),node));
+        }
+
+        Stack<Node> discovered = new Stack<>();
+
+        boolean isDone = false;
+        while(!isDone){
+            Node moveNode = s.currentNode();
+            discovered.push(moveNode);
+            int min = 0;
+            // Look through list of available neighbors and compare their cost
+            // picks the node with the lowest cost and not in discovered list
+            for(Node node: s.currentNode().getNeighbors()){
+                if(min == 0){
+                    min = aMap.get(node);
+                    moveNode = node;}
+                if(aMap.get(node) < min && (!node.equals((discovered.peek())))){
+                    System.out.println(discovered);
+                    moveNode = node;
+                    System.out.println(moveNode);
+                }
+            }
+            s.moveTo(moveNode);
+            if(s.currentNode().getTile().type() == Tile.TileType.RING){
+                isDone = true;
+            }
+        }
+    }
+
+    public static Integer nodeCost(Node start, Node exit, Node currNode){
+        // Find manhattan distance from start to node
+        int gCost = Math.abs((start.getTile().row() - currNode.getTile().row()) +
+                (start.getTile().column() - currNode.getTile().column()));
+        // Find manhattan distance from exit to node
+        int hCost = Math.abs((exit.getTile().row() - currNode.getTile().row()) +
+                (exit.getTile().column() - currNode.getTile().column()));
+
+        return gCost + hCost;
     }
 
 
